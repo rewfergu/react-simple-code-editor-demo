e14b10a212cb4567f7480ce678a0b7014a0d7b90
Shea Scott <sheadscott@gmail.com>
2023-07-21 18:07:45 -0500

    Initial commit

---
+++ remix/app/root.tsx
@@ -0,0 +1,37 @@
+import { cssBundleHref } from '@remix-run/css-bundle'
+import type { LinksFunction } from '@remix-run/node'
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react'
+
+import cestyles from './styles/code-editor.css'
+// styles is now something like /build/global-AE33KB2.css
+
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+  { rel: 'stylesheet', href: cestyles },
+]
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  )
+}
---
+++ remix/app/routes/code-editor.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import Editor from 'react-simple-code-editor'
+import { highlight, languages } from 'prismjs'
+import 'prismjs/components/prism-cooklang'
+
+const defaultCooklang = `Mix @flour{1%cup} and @water{1/2%cup} in a #large bowl{} and serve.`
+
+export default function CooklangEditor() {
+  const [cooklang, setCooklang] = useState(defaultCooklang)
+  return (
+    <div id="cooklang-editor">
+      <Editor
+        value={cooklang}
+        onValueChange={(cooklang) => setCooklang(cooklang)}
+        highlight={(cooklang) =>
+          highlight(cooklang, languages.cooklang, 'cooklang')
+        }
+        padding={16}
+        preClassName="language-cooklang"
+        style={{
+          color: '#fff',
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: 14,
+          backgroundColor: '#021622',
+        }}
+      />
+    </div>
+  )
+}
